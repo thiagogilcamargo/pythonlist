21b620b8e6606f2d084654d25acaf469a2366782
Thiago Gil Camargo <126835572+thiagogilcamargo@users.noreply.github.com>
2023-03-16 12:40:42 -0300

    Update exercicios Python.py

--- exercicios Python.py
+++ exercicios Python.py
@@ -1,21 +1,21 @@
-EXERCICIOS
+#EXERCICIOS
 
-2.
+#2.
 nome = input("Digite seu nome: ")
 sobrenome = input("Digite seu sobrenome: ")
 print(sobrenome, nome)
 
-Código em Python:
+
  
- 3.
+# 3.
 nome = input("Digite seu nome: ")
 ano_nascimento = int(input("Digite o ano de seu nascimento: "))
 idade = 2022 - ano_nascimento
 print(nome, "tem (ou terá)", idade, "anos")
 
-Código em Python:
 
-4.
+
+#4.
 a = int(input("Digite o primeiro número: "))
 b = int(input("Digite o segundo número: "))
 soma = a + b
@@ -27,17 +27,17 @@ print("Multiplicação:", multiplicacao)
 print("Divisão inteira:", divisao_inteira)
 print("Resto da divisão inteira:", resto_divisao_inteira)
 
-Código em Python:
 
-5.
+
+#5.
 x = int(input("Digite o valor de x: "))
 y = int(input("Digite o valor de y: "))
 resultado = x ** y
 print(resultado)
 
-Código em Python:
 
-6.
+
+#6.
 raio = float(input("Digite o raio do círculo: "))
 pi = 3.141592
 area = pi * raio ** 2
@@ -45,28 +45,27 @@ perimetro = 2 * pi * raio
 print("Área:", area)
 print("Perímetro:", perimetro)
 
-Código em Python:
 
-7.
+
+#7.
 numero = int(input("Digite um número de 0 a 99: "))
 dezena = numero // 10
 unidade = numero % 10
 print("Dígito das dezenas:", dezena)
 print("Dígito das unidades:", unidade)
 
-Código em Python:
 
-8.
+
+#8.
 camisas = int(input("Digite o número de camisas: "))
 calcas = int(input("Digite o número de calças: "))
 sapatos = int(input("Digite o número de pares de sapato: "))
 combinacoes = camisas * calcas * sapatos
 print("Número de combinações possíveis:", combinacoes)
 
-Código em Python:
 
-9.
 
+#9.
 preco = float(input("Digite o preço do produto: "))
 percentual = float(input("Digite o percentual de desconto (positivo) ou aumento (negativo): "))
 
@@ -82,9 +81,8 @@ valor_aumento = preco * (percentual/100)
 novo_preco = preco + valor_aumento
 E mudar o texto que exibe o resultado para "Valor do aumento" e "Novo preço do produto".
 
-Código em Python:
 
-10.
+#10.
 distancia = float(input("Digite a distância percorrida em metros: "))
 tempo = float(input("Digite o tempo em segundos: "))
 
@@ -96,7 +94,7 @@ print(f"Velocidade média em km/h: {velocidade_km_h:.2f} km/h")
 
 Código em Python:
 
-11.
+#11.
 salario_anterior = float(input("Digite o salário anterior: "))
 salario_atual = float(input("Digite o salário atual: "))
 
@@ -106,7 +104,7 @@ print(f"Percentual de aumento: {percentual_aumento:.2f}%")
 
 Código em Python:
 
-12.
+#12.
 rm = int(input("Digite o RM do aluno: "))
 
 soma_digitos = 0
@@ -116,9 +114,8 @@ while rm > 0:
 
 print(f"A soma dos dígitos do RM é: {soma_digitos}")
 
-Código em Python:
 
-13.
+#13.
 nac = float(input("Digite a nota da NAC: "))
 am = float(input("Digite a nota do AM: "))
 ps = float(input("Digite a nota da PS: "))
@@ -127,9 +124,9 @@ media = (2 * nac + 3 * am + 5 * ps) / 10
 
 print(f"Média da disciplina: {media:.2f}")
 
-Código em Python:
 
-14.
+
+#14.
 valor_total = float(input("Digite o valor total do IPTU: "))
 valor_parcela = float(input("Digite o valor de cada parcela: "))
 
